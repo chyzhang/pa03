a3c7c302a287167627c0cc9ec05ff13fbc5401e6
chyzhang@brandeis.edu <19940119zhch>
2015-07-01 18:16:53 -0400

    pa04

--- client/firefly.js
+++ client/firefly.js
@@ -52,27 +52,39 @@ FireflyModel.prototype.update = function(dt){
     _.each(this.fireflyList, 
            function(f){
                 f.update(dt);
-                if(theNet.caught(f)){
+                if(f.alive==true &&theNet.caught(f)){
+                    luckyBall+=1;
+                    Session.set('counter', Session.get('counter') + 1);
                     f.alive =false;}
                 }
            );
     //this.fireflyList = _.filter
 }
 theModel = new FireflyModel();
-theModel.addFirefly(f1);
-theModel.addFirefly(f2);
-for(var i=0;i<100;i++){
+function renewModel(){
+theModel = new FireflyModel();
+for(var i=0;i<50;i++){
+    var myvx=Math.random()*10-5;
+    var myvy=(Math.random()-0.5)*10;
+    theModel.addFirefly(new Firefly(50,50,1,"black",myvx,myvy))
+
+}
+}
+for(var i=0;i<1000;i++){
     var myvx=Math.random()*10-5;
     var myvy=(Math.random()-0.5)*10;
     theModel.addFirefly(new Firefly(50,50,1,"black",myvx,myvy))
 
 }
 
-var counter =0;
+counter =0;
+luckyBall=0;
 var lastTime = (new Date()).getTime();
-
+function counterReturn(){
+    return counter;
+}
 function draw(){
-    var drawContext = gameboard.getContext("2d");
+   drawContext = gameboard.getContext("2d");
     
     drawContext.fillStyle = "yellow";
     drawContext.fillRect(0,0,gameboard.width,gameboard.height);
@@ -81,17 +93,23 @@ function draw(){
     
     _.each(theModel.fireflyList,
         function(f){
-        if(!f.alive)return;
+        if(!f.alive){return;}
         drawContext.strokeStyle = f.c;
         drawContext.beginPath();
         drawContext.arc(f.x*gameboard.width/100,
                         f.y*gameboard.height/100,
                         f.r*gameboard.width/100, 
                         0, 2*Math.PI,true);
+        drawContext.fillStyle = 'green';
+        drawContext.fill();
         drawContext.stroke();
     }
 );
-        
+    move(drawContext); 
+    
+}
+function move(drawContext){
+    
     var net =theModel.net;
 drawContext.strokeStyle =net.c;
 drawContext.beginPath();
@@ -101,9 +119,33 @@ drawContext.arc(net.x*gameboard.width/100,
                         0, 2*Math.PI,true);
 drawContext.stroke();
 
-    
-    
+   
 }
+
+
+daysLeft=0;
+timeCounted=11;
+var t;
+function count10(){
+    for(var i=0; i<8;i++){
+    console.log(timeleft);
+    timeleft-=1;
+    timedMsg()
+    }
+}
+keepGoing = true;
+function timedCount()
+ {   
+     if(keepGoing){
+     if(timeCounted>0){
+         timeCounted--;
+         t=setTimeout(function(){Session.set('timeleft', timeCounted);timedCount()},1000) ;
+     }else{
+        Session.set('timeleft',0);
+        running=false;
+     }
+     }
+ }
 
 
 function gameLoop(){
@@ -111,9 +153,10 @@ function gameLoop(){
     var dt = theTime-lastTime;
     lastTime =theTime;
     var fps = 1000/(dt);
-    theModel.update(dt/1000);
+    theModel.update(dt/40);
     draw();
-
+    $('#aaaaa').show();
+    $('#bbbbb').show();
 
     if(running)
         window.requestAnimationFrame(gameLoop);
@@ -122,23 +165,72 @@ function gameLoop(){
 drawIt=draw;
 var running =false;
 
+function resetCounter(){
+    counter=0;   
+}
+Session.setDefault('counter', 0);
+Session.setDefault('timeleft', 10);
+
+
 
 Template.firefly.events({
     
     "click #startgame":function(event){
         if(!running){
+            luckyBall=0;
+            Session.set('counter', 0); 
+            timeleft=-1;
+            Session.set('timeleft', 10);
             lastTime = (new Date()).getTime();
             running =true;
+            timedCount();
             gameLoop();
-            $("#startgame").html("Stop");
-        }else{
-            running = false;
-            $("#startgame").html("Start");
-
         }
+        
+    },
+    "click #pause":function(event){
+         if(!running){
+            lastTime = (new Date()).getTime();
+            running =true;
+            keepGoing=true;
+            timedCount();
+            gameLoop();
+            $("#pause").html("pause");
+         }else{
+             keepGoing=false;
+             timeCounted++;
+            running = false;
+            $("#pause").html("continue");
+         }
+    },
+    "click #restart":function(event){
+        renewModel();
+        luckyBall=0;
+        Session.set('counter', 0); 
+        timeCounted=11;
+        Session.set('timeleft', 11);
+        lastTime = (new Date()).getTime();
+        running =true;
+        timedCount();
+        gameLoop();
+        
+    },
+    "click #Save":function(event){
+      event.preventDefault();
+
+		var ball = luckyBall;
+		console.log(JSON.stringify(this));
+
+            
+        Gamer1.insert({uid:Meteor.userId(), ball:ball}); 
     }
+    
 })
+
 Template.firefly.rendered = function(){
+    
+$('#aaaaa').hide();
+$('#bbbbb').hide();
 document.getElementById("gameboard").addEventListener('mousemove',
     function(e){
     if(running){
@@ -148,3 +240,18 @@ document.getElementById("gameboard").addEventListener('mousemove',
 }
 );
 }
+Template.firefly.helpers({
+	dead: function () {
+      return Session.get('counter');
+    },
+    Gamer1: function(){return Gamer1.find({}, {sort:{ball: -1}, limit:10});},
+    daysLeft:function(){ return Session.get('timeleft');
+    }
+})
+Template.iGamer1.helpers({
+	isTheGamer1: function(){return Meteor.userId() == this.uid}
+});
+
+Template.iGamer1.events({
+	"click .jbsapp-delete-icon": function(){Gamer1.remove(this._id);}
+})
--- client/subscriptions.js
+++ client/subscriptions.js
@@ -2,3 +2,4 @@ Meteor.subscribe("theProfiles");
 Meteor.subscribe("userData");
 Meteor.subscribe("theColors");
 Meteor.subscribe("theChat");
+Meteor.subscribe("theGamer1");
